f66a92853bae6523ba129d09e37666bd89f749a8
mariadalpuimG <79101889+mariadalpuimG@users.noreply.github.com>
2022-02-13 22:20:59 +0000

    burguer button is working with JavaScript q

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 console.log('Hello');
 
-const toHide = document.getElementById("buttonContent");
-const btn = document.getElementById("collapsible");
-btn.onclick = function () {
-  if (toHide.style.display !== "none") {
-    toHide.style.display = "none";
-  } else {
-    toHide.style.display = "block";
+// function to control expand/hide the navbar options when clicked. 
+// create variables global scope to invoke them in functions and in listeners
+const toHide = document.querySelector("#btnOptions");
+const btn = document.querySelector('#burguerBtn');
+// if currently is hidden/none - click sets to block; if block - click sets to none.
+  expandNavBar = () => {
+    if (toHide.style.display !== "none") {
+          toHide.style.display = "none";
+        } else {
+          toHide.style.display = "flex";
+        }
+    }
+
+// event listener for when we click the variable, it runs function
+btn.addEventListener('click', expandNavBar)
+// this would do the same if didn't have/called the variable btn
+// document.querySelector('#burguerBtn').addEventListener('click', expandNavBar)
+
+// this makes the bar being hidden by default.
+expandNavBar();
+
+// eventlistener in each of the buttons inside nav bar, and when one is clicked
+// button disappears/sets to none
+hideNavBar = () => {
+  toHide.style.display = "none";
   }
-};
 
-// why do I need to click twice?
-
-// it's working. now I want to add that it hides
-// when we select one of the options. prob need
-// and if or a loop/
+document.querySelector('#btnOp1').addEventListener('click', hideNavBar)
+document.querySelector('#btnOp2').addEventListener('click', hideNavBar)
+document.querySelector('#btnOp3').addEventListener('click', hideNavBar)
